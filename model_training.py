233bb17413553ffedfc35acfa45c993320578c5b
ericspencer00 <espencer2@luc.edu>
2025-02-06 16:49:11 -0600

    Batch Normalization

--- model_training.py
+++ model_training.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Rescaling
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Rescaling, BatchNormalization, Dropout
 from tensorflow.keras.utils import to_categorical
 
 def load_collected_data(data_dir):
@@ -34,12 +34,20 @@ def preprocess_collected_data(X, y):
 def build_classification_model(input_shape, num_classes):
     model = Sequential([
         Rescaling(1./255, input_shape=input_shape),
-        Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        Conv2D(32, (3, 3), activation='relu'),
+        BatchNormalization(),
         MaxPooling2D((2, 2)),
+        Dropout(0.25),
+        
         Conv2D(64, (3, 3), activation='relu'),
+        BatchNormalization(),
         MaxPooling2D((2, 2)),
+        Dropout(0.25),
+        
         Flatten(),
         Dense(128, activation='relu'),
+        BatchNormalization(),
+        Dropout(0.5),
         Dense(num_classes, activation='softmax')
     ])
     model.compile(optimizer='adam',
